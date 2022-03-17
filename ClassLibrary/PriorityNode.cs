6be375422154bd8a6cef422ebe0fbd65cf30ad7c
DiegoBautista <da10bautista@gmail.com>
2022-03-17 08:41:30 -0600

    Creación de la librearía de clases, y el objeto PriorityNode

---
+++ ClassLibrary/PriorityNode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class PriorityNode <T>
+    {
+        public T Value { get; set; }
+        PriorityNode<T> Left;
+        PriorityNode<T> Rigth;
+
+        public PriorityNode(T value)
+        {
+            this.Value = value;
+            this.Left = null;
+            this.Rigth = null;
+        }
+    }
+}
